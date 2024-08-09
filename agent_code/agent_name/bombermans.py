4df386500232f51853c66bc5f0de0001c5173cd0
luwo9 <126659866+luwo9@users.noreply.github.com>
2024-08-09 11:18:12 +0200

    bundles more flexible loading for multiple runs (Saving agents #7, Multiple runs of main.py #10)

--- agent_code/agent_name/bombermans.py
+++ agent_code/agent_name/bombermans.py
@@ -23,7 +23,7 @@ WIDTH, HEIGHT = 17, 17
 
 N_ACTIONS = 6
 
-ACTINS_MAP = {
+ACTIONS_MAP = {
     0: 'UP',
     1: 'RIGHT',
     2: 'DOWN',
@@ -32,23 +32,68 @@ ACTINS_MAP = {
     5: 'BOMB'
 }
 
-ACTIONS_INV_MAP = {v: k for k, v in ACTINS_MAP.items()}
+ACTIONS_INV_MAP = {v: k for k, v in ACTIONS_MAP.items()}
 
 
 class BombermanBundle(ABC):
     """
     Abstract base class for Bombermans agents.
 
-    Subclasses should bundle together all components and must construct a QLearningAgent and a Rewarder that are then used to play the game.
+    Intended to bundle an agent together with all its necessary components.
+
+    The bundle creates the components and is responsible for:
+    - Saving and loading the components (methods _state_dict and _load_state_dict)
+    - Assembling them to a q-agent and a rewarder (property _q_agent and _rewarder)
+
+    The q-agent and the rewarder are automatically used to provide the act and game_events_occurred methods to play the game.
+
+    The bundle also keeps track of whether it is in training mode or not. A newly created bundle is always in training mode.
+    When loading a bundle one can specify whether loading for training or playing and the mode will be set accordingly.
+    This boolean flag is also passed on to _load_state_dict such that only components that are needed for training can be loaded.
+
+    Only when loaded in training mode:
+    - The agent will explore in its actions.
+    - The save() method will save the state of the agent to a file.
+    - A call new_run() indicates that a new training run starts.
+
+    Starting a new run increments the counter _n_training_runs.
+
+    This counter can be used in _load_state_dict to make changes to the components.
+    This is useful if, e.g. a training run is completed but now training should be succeded in a changed environment,
+    where e.g. the rewarder needs change.
+
+    Finally, the save() method can be called with the final=True flag to indicate that this is the final save that can never be retrained.
+    This is passed to _state_dict(), that can then throw away everything only needed for training to rduce file size.
+    After this was done the agent is always loaded with training=False.
     """
 
-    def __init__(self, training: bool = False):
+    def __init__(self):
         """
         Initializes all components.
-
-        :param training: bool, optional (default=False), whether the agent is used for training or playing
         """
-        self._training_mode = training
+        self._training_mode = True
+        self._n_training_runs = 0
+        self._was_finalized = False
+
+    @abstractmethod
+    def _state_dict(self, final: bool) -> dict:
+        """
+        Returns the state of the agent as a dictionary.
+
+        :param final: bool, whether this is the final save that can never be retrained
+        :return: dict
+        """
+        pass
+
+    @abstractmethod
+    def _load_state_dict(self, state_dict: dict, training: bool):
+        """
+        Loads the state of the agent from a dictionary.
+
+        :param state_dict: dict
+        :param training: bool, whether the agent is used for training or playing
+        """
+        pass
 
     @property
     @abstractmethod
@@ -70,6 +115,13 @@ class BombermanBundle(ABC):
         """
         pass
 
+    def new_run(self):
+        """
+        If in training mode, indicate a new training run starts.
+        """
+        if self._training_mode:
+            self._n_training_runs += 1
+
     def act(self, game_state: dict) -> str:
         """
         Returns the action that the agent will take in the current state.
@@ -77,7 +129,7 @@ class BombermanBundle(ABC):
         :param game_state: dict
         :return: str
         """
-        return ACTINS_MAP[self._q_agent.get_action(game_state, self._training_mode)]
+        return ACTIONS_MAP[self._q_agent.get_action(game_state, self._training_mode)]
     
     def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict|None, events: list):
         """
@@ -92,17 +144,19 @@ class BombermanBundle(ABC):
         reward = self._rewarder.compute_reward_from_events(events)
         self._q_agent.update(old_game_state, self_action, reward, new_game_state)
 
-    def save(self, path: str):
+    def save(self, path: str, final: bool = False):
         """
-        Saves the agent to a file.
+        Saves the agent to a file, if in training mode.
 
         :param path: str
+        :param final: bool, optional (default=False), whether this is the final save that can never be retrained
         """
-        with open(path, 'wb') as f:
-            pickle.dump(self._state_dict(), f)
+        if self._training_mode:
+            with open(path, 'wb') as f:
+                pickle.dump(self._state_dict(final), f)
+                self._was_finalized = final
 
-    @classmethod
-    def load(cls, path: str, training: bool = False) -> BombermanBundle:
+    def load(self, path: str, training: bool = False):
         """
         Loads the agent from a file.
 
@@ -112,30 +166,10 @@ class BombermanBundle(ABC):
         """
         with open(path, 'rb') as f:
             state_dict = pickle.load(f)
-        
-        agent = cls(training)
-        agent._load_state_dict(state_dict)
-        return agent
-    
-    def _state_dict(self) -> dict:
-        """
-        Returns the state of the agent as a dictionary.
 
-        :return: dict
-        """
-        return {
-            "q_agent": self._q_agent.state_dict(),
-            "rewarder": self._rewarder.state_dict()
-        }
-    
-    def _load_state_dict(self, state_dict: dict):
-        """
-        Loads the state of the agent from a dictionary.
-
-        :param state_dict: dict
-        """
-        self._q_agent.load_state_dict(state_dict["q_agent"])
-        self._rewarder.load_state_dict(state_dict["rewarder"])
+        self._training_mode = training
+        training = self._training_mode and not self._was_finalized
+        self._load_state_dict(state_dict, training)
 
 
 class VectorMLPSimple(BombermanBundle):
@@ -155,32 +189,32 @@ class VectorMLPSimple(BombermanBundle):
 
         :param training: bool, optional (default=False), whether the agent is used for training or playing
         """
-        super().__init__(training)
+        super().__init__()
 
         # Exploration and exploitation
-        ex_ex_handler = policy_modifiers.EpsilonGreedy(0.3)
+        self._ex_ex_handler = policy_modifiers.EpsilonGreedy(0.3)
 
         # Q-handler
 
         # Regression model
-        transformer = transforms.AllfieldsFlat(WIDTH, HEIGHT)
-        neural_net = networks.NeuralNetwork(WIDTH*HEIGHT*8, [512, 256, 128], N_ACTIONS)
-        loss = nn.SmoothL1Loss()
-        optimizer = optim.RAdam(neural_net.parameters(), lr=0.0001)
+        self._transformer = transforms.AllfieldsFlat(WIDTH, HEIGHT)
+        self._neural_net = networks.NeuralNetwork(WIDTH*HEIGHT*8, [512, 256, 128], N_ACTIONS)
+        self._loss = nn.SmoothL1Loss()
+        self._optimizer = optim.RAdam(self._neural_net.parameters(), lr=0.0001)
 
-        regression_model = regression_models.NeuralNetworkVectorQRM(neural_net, transformer, loss, optimizer, N_ACTIONS, "cpu")
+        self._regression_model = regression_models.NeuralNetworkVectorQRM(self._neural_net, self._transformer, self._loss, self._optimizer, N_ACTIONS, "cpu")
 
-        training_memory_use = training_memory.TrainingMemory(100)
+        self._training_memory_use = training_memory.TrainingMemory(100)
 
-        sampler = samplers.RandomSampler()
+        self._sampler = samplers.RandomSampler()
 
-        sample_step = (3, 64, sampler)
-        sample_round = (1, 256, sampler)
+        sample_step = (3, 64, self._sampler)
+        sample_round = (1, 256, self._sampler)
 
-        q_handler = qhandler.RegressionQHandler(regression_model, training_memory_use, N_ACTIONS, sample_step, sample_round)
+        self._q_handler = qhandler.RegressionQHandler(self._regression_model, self._training_memory_use, N_ACTIONS, sample_step, sample_round)
 
         # Q Agent
-        self._q_agent_ = qagents.SimpleQLearningAgent(q_handler, ex_ex_handler)
+        self._q_agent_ = qagents.SimpleQLearningAgent(self._q_handler, self._ex_ex_handler)
 
         # Rewarder
         self._rewarder_ = rewarders.SimpleRewarder()
@@ -192,3 +226,51 @@ class VectorMLPSimple(BombermanBundle):
     @property
     def _rewarder(self) -> rewarders.Rewarder:
         return self._rewarder_
+    
+    def _state_dict(self, final: bool) -> dict:
+        # Save all components manually
+        out_dict = {
+                "training_mode": self._training_mode,
+                "n_training_runs": self._n_training_runs,
+                "was_finalized": self._was_finalized,
+                "ex_ex_handler": self._ex_ex_handler.state_dict(),
+                "transformer": self._transformer.state_dict(),
+                "neural_net": self._neural_net.state_dict(),
+                "regression_model": self._regression_model.state_dict(),
+                "q_agent": self._q_agent_.state_dict(),
+                "q_handler": self._q_handler.state_dict(),
+                "rewarder": self._rewarder_.state_dict()
+            }
+        
+        if not final:
+            out_dict.update({
+                "loss": self._loss.state_dict(),
+                "optimizer": self._optimizer.state_dict(),
+                "training_memory_use": self._training_memory_use.state_dict(),
+                "sampler": self._sampler.state_dict()
+            })
+
+        return out_dict
+        
+    def _load_state_dict(self, state_dict: dict, training: bool):
+        # Load all components manually
+        self._training_mode = state_dict["training_mode"]
+        self._n_training_runs = state_dict["n_training_runs"]
+        self._was_finalized = state_dict["was_finalized"]
+        if training:
+            self._loss.load_state_dict(state_dict["loss"])
+            self._optimizer.load_state_dict(state_dict["optimizer"])
+            self._training_memory_use.load_state_dict(state_dict["training_memory_use"])
+            self._sampler.load_state_dict(state_dict["sampler"])
+            self._rewarder_.load_state_dict(state_dict["rewarder"])
+        
+        # Always loaded
+        self._ex_ex_handler.load_state_dict(state_dict["ex_ex_handler"])
+        self._transformer.load_state_dict(state_dict["transformer"])
+        self._neural_net.load_state_dict(state_dict["neural_net"])
+        self._regression_model.load_state_dict(state_dict["regression_model"])
+        self._q_agent_.load_state_dict(state_dict["q_agent"])
+        self._q_handler.load_state_dict(state_dict["q_handler"])
+
+        # if self._n_training_runs > 0:
+        #     self._rewarder_ = rewarders.TemplateRewarder()
