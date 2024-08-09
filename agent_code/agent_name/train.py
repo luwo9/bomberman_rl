c4bc0fc9eac6b0ed4a739aa5cbe2c1c4832b6f38
luwo9 <126659866+luwo9@users.noreply.github.com>
2024-08-09 11:18:51 +0200

    adjust callbacks and improve

--- agent_code/agent_name/train.py
+++ agent_code/agent_name/train.py
@@ -1,11 +1,16 @@
 from typing import List # list works as type annotation for new enough python versions, but since the final version is unknown, we use List
 
+from .bombermans import BombermanBundle
+
 def setup_training(self):
     pass
 
 def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
-    self.model.game_events_occurred(old_game_state, self_action, new_game_state, events)
+    model: BombermanBundle = self.model
+    model.game_events_occurred(old_game_state, self_action, new_game_state, events)
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
-    self.model.game_events_occurred(last_game_state, last_action, None, events)
-    self.model.save("saved-model.pt")
+    model: BombermanBundle = self.model
+    model.game_events_occurred(last_game_state, last_action, None, events)
+    if last_game_state["round"] % 100 == 0:
+        model.save(self.file_name)
