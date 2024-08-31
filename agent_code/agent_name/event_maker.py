0997361a72a8ba7f404600fa0fc9f668678ae5f0
luwo9 <126659866+luwo9@users.noreply.github.com>
2024-08-31 15:55:32 +0200

    Bugfix

--- agent_code/agent_name/event_maker.py
+++ agent_code/agent_name/event_maker.py
@@ -2,8 +2,9 @@
 Used to create custom events for the agent from other events or game states.
 They can be used to combine events or create new events based on the game states.
 """
-import events as e
 import numpy as np
+
+import events as e
 from .bomberman_base import get_blast_coords
 
 place_map = {1: e.WON_GAME, 2: e.SECOND_PLACE, 3: e.THIRD_PLACE, 4: e.LOST_GAME}
@@ -79,24 +80,21 @@ class EventMaker:
         else:
             events.append(e.NO_BOMB_RANGE)
 
-        # Negations
-        if not e.COIN_COLLECTED in events:
-            events.append(e.NO_COIN)
-
-        if not e.CRATE_DESTROYED in events:
-            events.append(e.NO_CRATE)
-
-        
         if (self_action == 'BOMB'):
-            x,y = np.array(get_blast_coords(own_x, own_y).T)
-            crate_count = np.sum(old_game_state["field"][x,y] == 1)
+            own_x, own_y = own_position
+            x, y = np.array(get_blast_coords(own_x, own_y).T)
+            crate_count = np.sum(old_game_state["field"][x, y] == 1)
             
             filtered_keys = [key for key in crates_in_bomb_range_map.keys() if key <= crate_count]
             if filtered_keys:
                 largest_key = max(filtered_keys)
                 events.append(crates_in_bomb_range_map[largest_key])
 
-            
+        # Negations
+        if not e.COIN_COLLECTED in events:
+            events.append(e.NO_COIN)
 
+        if not e.CRATE_DESTROYED in events:
+            events.append(e.NO_CRATE)          
 
         return events
